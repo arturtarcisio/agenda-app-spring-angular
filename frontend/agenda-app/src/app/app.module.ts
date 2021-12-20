00b203eb22115df1e01e1e1237819c770b9a7eba
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-12-19 21:29:58 -0300

    Melhorias no layout

--- frontend/agenda-app/src/app/app.module.ts
+++ frontend/agenda-app/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatTableModule } from '@angular/material/table'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ContatoComponent } from './contato/contato.component'
@@ -29,6 +33,10 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatToolbarModule,
     MatInputModule,
+    MatTableModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [
